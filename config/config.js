e21d0c4a0832674b48e489e24763310ad52fcf25
zhenglipeng <zhenglipeng@sensetime.com>
2021-10-08 18:18:57 +0800

    github-ci

--- config/config.js
+++ config/config.js
@@ -1,6 +1,34 @@
 import routes from './routes';
 
+let publicPath = '/jrpg/';
+if (process.env.NODE_ENV === 'production') {
+  publicPath = 'https://cdn.jsdelivr.net/gh/lanyuechen/jrpg@gh-pages/';
+}
+
 // ref: https://umijs.org/config/
 export default {
+  title: 'JRPG',
+  history: {
+    type: 'hash'
+  },
+  hash: true,
+  publicPath,  // jsdelivr cdn 加速
   routes,
+  define: {
+    PUBLIC_PATH: publicPath,
+  },
+  // dynamicImport: {
+  //   loading: '@/components/loading',
+  // },
+  nodeModulesTransform: {
+    type: 'none',
+    exclude: [],
+  },
+  targets: {
+    chrome: 80,
+    firefox: false,
+    safari: false,
+    edge: false,
+    ios: false,
+  },
 }
--- src/core/utils/utils.js
+++ src/core/utils/utils.js
@@ -48,3 +48,39 @@ export function hitTestRectangle(r1, r2) {
   //`hit` will be either `true` or `false`
   return hit;
 };
+
+export function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+export function contain(sprite, container) {
+
+  let collision = undefined;
+
+  //Left
+  if (sprite.x < container.x) {
+    sprite.x = container.x;
+    collision = "left";
+  }
+
+  //Top
+  if (sprite.y < container.y) {
+    sprite.y = container.y;
+    collision = "top";
+  }
+
+  //Right
+  if (sprite.x + sprite.width > container.width) {
+    sprite.x = container.width - sprite.width;
+    collision = "right";
+  }
+
+  //Bottom
+  if (sprite.y + sprite.height > container.height) {
+    sprite.y = container.height - sprite.height;
+    collision = "bottom";
+  }
+
+  //Return the `collision` value
+  return collision;
+}
